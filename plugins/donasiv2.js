f21470dece0db236d536d9b3e95386ba6dfcfdb3
FairusAnjayy <94828449+FairusAnjay@users.noreply.github.com>
2022-10-30 01:46:22 +0700

    Add files via upload

---
+++ plugins/donasiv2.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, usedPrefix }) => {
+
+    let img = fs.readFileSync('./thumbnail.jpg');
+    let img2 = 'https://telegra.ph/file/e6184fe12e3f0b42b418d.mp4'
+    return conn.sendButton(m.chat, 'Donasi Buat Gift Alok :D\n\nPulsa Indosat: 085649064565', donate, img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: "https://instagram.com/fairuzzgapunyaduit",
+                mediaType: "VIDEO",
+                title: 'Donasi To FairuzXD',
+                body: wm,
+                previewType: 0,
+                thumbnail: img
+            }
+        }
+    })
+}
+handler.help = ['donasiv2']
+
+handler.tags = ['info']
+
+handler.command = /^(donasiv2|donatev2)$/i
+
+
+export default handler
